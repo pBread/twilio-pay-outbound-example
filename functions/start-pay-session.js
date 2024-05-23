607bbec5f98e7522bebd84362ad260358dc8b94f
pBread <4473570+pBread@users.noreply.github.com>
2024-05-23 09:55:33 -0600

    minor

--- functions/start-pay-session.js
+++ functions/start-pay-session.js
@@ -1,12 +1,12 @@
 const { ACCOUNT_SID, AUTH_TOKEN } = process.env;
 const client = require("twilio")(ACCOUNT_SID, AUTH_TOKEN);
 
-exports.handler = async function handler(ctx, { request, ...event }, callback) {
+exports.handler = async function wrapper(ctx, { request, ...event }, callback) {
   try {
-    await startPaySession(ctx, event, callback);
+    await handler(ctx, event, callback);
   } catch (error) {
     callback(error);
   }
 };
 
-async function startPaySession(ctx, event, callback) {}
+async function handler(ctx, event, callback) {}
