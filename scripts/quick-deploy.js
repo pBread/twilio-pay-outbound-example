036f3819371946cffa913e0b7b69e0a425092524
pBread <4473570+pBread@users.noreply.github.com>
2024-05-23 12:23:36 -0600

    added dotenv

---
+++ scripts/quick-deploy.js
@@ -0,0 +1,11 @@
+/****************************************************
+ Quick Deploy Script
+
+****************************************************/
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: true,
+});
