ec13ee0b23ce478c0b5277d2e03fde4d81f50c45
pBread <4473570+pBread@users.noreply.github.com>
2024-05-23 14:02:48 -0600

    added api key

--- scripts/quick-deploy.js
+++ scripts/quick-deploy.js
@@ -31,6 +31,7 @@ const {
 } = process.env;
 
 let state = {
+  warnings: [],
   accountSid: ACCOUNT_SID,
   authToken: AUTH_TOKEN,
 
@@ -50,23 +51,82 @@ let client = require("twilio")(ACCOUNT_SID, AUTH_TOKEN);
 
 (async () => {
   await manageAccount();
-  await ask("Howdy");
-  await render();
+  await manageApiKey();
+
   rl.close();
 })();
 
 async function manageAccount() {
-  const account = await client.api.v2010.accounts(ACCOUNT_SID).fetch();
+  if (!ACCOUNT_SID || !AUTH_TOKEN)
+    throw Error(
+      `Missing required env variables: ACCOUNT_SID and/or AUTH_TOKEN`
+    );
 
-  await setState({ account });
+  try {
+    const account = await client.api.v2010.accounts(ACCOUNT_SID).fetch();
+    await setState({ account });
+  } catch (error) {
+    console.error(
+      "Unable to fetch account. Check your ACCOUNT_SID & AUTH_TOKEN"
+    );
+    throw error;
+  }
+}
+
+async function manageApiKey() {
+  if (state.apiKey && state.apiSecret) return;
+
+  const answer = await ask(
+    `You have not provided an API Key. Would you like to create an API key now? (y or n, default = y)`
+  );
+
+  const doCreateApiKey = evalYesNo(answer);
+
+  if (!doCreateApiKey)
+    setState({
+      warnings: state.warnings.concat(`\
+You have not defined an API key. You will need to create one and add the TWILIO_API_KEY & TWILIO_API_SECRET to environment variables to your Twilio Function.`),
+    });
+
+  try {
+    const apiKeyResult = await client.newKeys.create({
+      friendlyName: "twilio-pay-example",
+    });
+
+    const apiKey = apiKeyResult.sid;
+    const apiSecret = apiKeyResult.secret;
+
+    setState({ apiKey, apiSecret });
+  } catch (error) {
+    console.error("Unable to create API Key");
+    throw error;
+  }
+}
+
+function evalYesNo(answer) {
+  if (!answer) answer = "y";
+  answer = answer.trim().toLowerCase();
+
+  if (answer === "y" || answer === "yes") return true;
+  if (answer === "n" || answer === "no") return false;
+
+  throw Error("Invalid Reponse");
 }
 
 async function render() {
-  //   console.clear();
+  console.clear();
   util.printTable([
     ["Account SID", state.accountSid],
     ["Account Name", state.account?.friendlyName],
     "separator",
     ["Has API Key", !!state.apiKey && !!state.apiSecret],
   ]);
+
+  for (let i = 0; i < state.warnings.length; i++) {
+    if (i === 0) console.log("\n\nMessages:");
+    const warning = `(${i + 1})  ` + state.warnings[i];
+    console.log(warning);
+  }
+
+  console.log("\n\n");
 }
