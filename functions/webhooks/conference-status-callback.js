32dc39ab08aa1eaa46b33242b8b4e0c73637c275
pBread <4473570+pBread@users.noreply.github.com>
2024-05-23 10:13:19 -0600

    added wrapper

--- functions/webhooks/conference-status-callback.js
+++ functions/webhooks/conference-status-callback.js
@@ -3,13 +3,34 @@ const client = require("twilio")(ACCOUNT_SID, AUTH_TOKEN);
 
 exports.handler = async function wrapper(ctx, event, callback) {
   try {
-    return handler(ctx, event, callback);
+    switch (event.CallStatus) {
+      case "completed":
+        return onCallComplete(ctx, event, callback);
+
+      case "in-progress":
+        return onUserAnswer(ctx, event, callback);
+
+      case "initiated":
+      case "ringing":
+        const fns = Runtime.getFunctions();
+        const sync = require(fns["shared/sync"].path);
+        const customerPN = event.To;
+        // update session SyncDoc with call status, which is available in user interface
+        await sync.updateSession(customerPN, { status: event.CallStatus });
+
+      default:
+        callback(null, {});
+    }
   } catch (error) {
     console.error(error);
     callback(error);
   }
 };
 
-async function handler(ctx, event, callback) {
+async function onUserAnswer(ctx, event, callback) {
+  callback(null, {});
+}
+
+async function onCallComplete(ctx, event, callback) {
   callback(null, {});
 }
